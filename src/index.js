dcbceb0a8e544f7ecc9f0dbdebe1a823b896e7f2
aaltmark <adamaltmark@gmail.com>
2020-07-28 11:00:10 -0400

    bootstrapped

---
+++ src/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+    const cardBody = document.getElementsByClassName('card-body')
+    const cardH = document.getElementsByClassName('card h-100')
+    const col = document.getElementsByClassName("col-md-4 mb-5")
+    const row = document.getElementsByClassName("row")
+    console.dir(row)
+})
