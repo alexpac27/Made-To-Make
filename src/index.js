1b383ba796042a1190b045d751646d89c7682da6
aromsa <annacbateman@gmail.com>
2020-07-28 15:23:48 -0400

    rendered items

--- src/index.js
+++ src/index.js
@@ -2,6 +2,38 @@ document.addEventListener("DOMContentLoaded", ()=> {
     const cardBody = document.getElementsByClassName('card-body')
     const cardH = document.getElementsByClassName('card h-100')
     const col = document.getElementsByClassName("col-md-4 mb-5")
-    const row = document.getElementsByClassName("row")
-    console.dir(row)
+    const itemsContainer = document.getElementById("items-container")
+    const itemsUrl = "http://localhost:3000/api/v1/items"
+    const addButton = document.getElementById("add-button")
+    console.log(addButton)
+
+    function fetchItems(){
+    fetch(itemsUrl)
+    .then(resp => resp.json())
+    .then(items => renderItems(items))
+    }
+
+    function renderItems(items){
+      items.forEach(item => renderOneItem(item))
+    }
+
+    function renderOneItem(item){
+      const itemCard = document.createElement("div")
+      itemCard.className = "col-md-4 mb-5"
+      itemCard.innerHTML = `
+        <div class="card h-100">
+          <div class="card-body">
+           
+            <h2 class="card-title">${item.name}</h2>
+            <p class="card-text">${item.description}</p>
+            <p class="card-text">${item.price}</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary btn-sm">More Info</a>
+          </div>
+        </div>`
+        itemsContainer.append(itemCard)
+    }
+
+    fetchItems()
 })
